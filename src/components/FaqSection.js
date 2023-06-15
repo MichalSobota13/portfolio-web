5895a8e435125c909564956eb9e02fce5aa7b30a
MichalSobota13 <michalsobota13@gmail.com>
2023-06-15 10:53:32 +0200

    services and about section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta
+            dolor quae ex distinctio incidunt quidem hic, tempore animi,
+            deserunt quis corporis inventore perspiciatis quisquam!
+            Necessitatibus nam velit consequatur earum aut?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta
+            dolor quae ex distinctio incidunt quidem hic, tempore animi,
+            deserunt quis corporis inventore perspiciatis quisquam!
+            Necessitatibus nam velit consequatur earum aut?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Differrent Payment Methods</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta
+            dolor quae ex distinctio incidunt quidem hic, tempore animi,
+            deserunt quis corporis inventore perspiciatis quisquam!
+            Necessitatibus nam velit consequatur earum aut?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Products do you offer.</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta
+            dolor quae ex distinctio incidunt quidem hic, tempore animi,
+            deserunt quis corporis inventore perspiciatis quisquam!
+            Necessitatibus nam velit consequatur earum aut?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
